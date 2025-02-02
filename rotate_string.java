bd73b914bb8da66bf83fcfa32c061d7703c27533
yasar654 <yasaralam63@gmail.com>
2025-02-02 13:39:58 +0530

    Create rotate_string.java

---
+++ rotate_string.java
@@ -0,0 +1,13 @@
+// rotate string
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        // Check if lengths are equal
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        // Concatenate s with itself and check if goal is a substring
+        String comb = s + s;
+        return comb.contains(goal);
+    }
+}
